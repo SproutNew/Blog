25e44b14e2ceffb4219a37aa0d9c96595410fed4
SproutNew <627476366@qq.com>
2018-06-17 19:12:53 +0800

    Blog

---
+++ src/blog/junit/UserTest.java
@@ -0,0 +1,30 @@
+package blog.junit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.sql.Connection;
+import org.junit.Test;
+
+import blog.dao.UserDao;
+import blog.daoImpl.UserDaoImpl;
+import blog.model.User;
+
+public class UserTest {
+	
+	@Test
+	public void testLogin() throws Exception{			
+		String name="Êý¾Ý¿âÃû";
+		String password="ÃÜÂë";
+		
+		UserDao userDao = UserDaoImpl.getInstance();
+		User user = userDao.login(name, password);
+		if(user!=null){
+			System.out.println(user.getUser_id());
+			System.out.println(user.getUser_name());
+			System.out.println(user.getUser_password());
+		}	else{
+			System.out.println("Login failed");
+		}
+		
+	}
+}
